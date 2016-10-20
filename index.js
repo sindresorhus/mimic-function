ba28a40c2dbe0b5ff4fac9d6351abf81de727aee
John-David Dalton <john.david.dalton@gmail.com>
2016-10-20 10:19:48 -0700

    Dry it out.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
 module.exports = (to, from) => {
-	if (from.name) {
-		Object.defineProperty(to, 'name', {
-			value: from.name,
-			configurable: true
-		});
-	}
-
-	Object.defineProperty(to, 'length', {
-		value: from.length,
-		configurable: true
-	});
-
-	if (from.displayName) {
-		to.displayName = from.displayName;
-	}
-
 	// TODO: use `Reflect.ownKeys()` when targeting Node.js 6
 	for (const prop of Object.getOwnPropertyNames(from)) {
 		Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
