3d4ea829f3561237b1e768d6e4ffdbb0ef5d8d1f
John-David Dalton <john.david.dalton@gmail.com>
2016-10-20 10:27:42 -0700

    Add support for copying symbols.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = (to, from) => {
 	}
 
 	// TODO: use `Reflect.ownKeys()` when targeting Node.js 6
-	for (const prop of Object.getOwnPropertyNames(from)) {
+	for (const prop of Object.getOwnPropertyNames(from).concat(Object.getOwnPropertySymbols(from))) {
 		Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
 	}
 };
