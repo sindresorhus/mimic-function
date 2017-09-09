e851aba732a95d7dd4024b79adf7ec23b652c570
tiagosmartinho <tiagoammartinho@gmail.com>
2017-09-09 17:09:04 +0100

    Increasing toString functionality

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
+const _ = require('lodash');
+
 module.exports = (to, from) => {
-	// TODO: use `Reflect.ownKeys()` when targeting Node.js 6
-	for (const prop of Object.getOwnPropertyNames(from).concat(Object.getOwnPropertySymbols(from))) {
-		Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
-	}
+    // TODO: use `Reflect.ownKeys()` when targeting Node.js 6
+    for (const prop of Object.getOwnPropertyNames(from).concat(Object.getOwnPropertySymbols(from))) {
+        Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
+    }
+
+    const aux = _.toString(to);
+    to.toString = function() {
+        const comment = `/* ${aux} */ \n ${from}`;
+        return comment;
+    };
 };
--- test.js
+++ test.js
@@ -1,21 +1,27 @@
+/* eslint new-cap: ["error", { "newIsCap": false }] */
 import test from 'ava';
 import m from './';
 
 test(t => {
-	const symbol = Symbol('🦄');
+    const symbol = Symbol('🦄');
 
-	function foo(bar) {} // eslint-disable-line no-unused-vars
-	foo.unicorn = '🦄';
-	foo[symbol] = '✨';
+    function foo(bar) {} // eslint-disable-line no-unused-vars
+    foo.unicorn = '🦄';
+    foo[symbol] = '✨';
 
-	function wrapper() {}
+    function wrapper() {
+        const x = 'hello world';
+        console.log(x);
+    }
 
-	t.is(foo.name, 'foo');
+    t.is(foo.name, 'foo');
 
-	m(wrapper, foo);
+    m(wrapper, foo);
 
-	t.is(wrapper.name, 'foo');
-	t.is(wrapper.length, 1);
-	t.is(wrapper.unicorn, '🦄');
-	t.is(wrapper[symbol], '✨');
+    t.is(wrapper.name, 'foo');
+    t.is(wrapper.length, 1);
+    t.is(wrapper.unicorn, '🦄');
+    t.is(wrapper[symbol], '✨');
+
+    console.log(wrapper.toString());
 });
