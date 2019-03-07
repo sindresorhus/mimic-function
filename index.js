eeaf3fb6e939e7908a18fd4c049528d771e13eee
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-07 18:53:32 +0000

    Add TypeScript definition (#14)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
-module.exports = (to, from) => {
+const mimicFn = (to, from) => {
 	for (const prop of Reflect.ownKeys(from)) {
 		Object.defineProperty(to, prop, Object.getOwnPropertyDescriptor(from, prop));
 	}
 
 	return to;
 };
+
+module.exports = mimicFn;
+module.exports.default = mimicFn;
