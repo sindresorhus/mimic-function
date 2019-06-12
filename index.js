77fe38abcfdd918fd053d84ad6a55e3588634b99
ehmicky <ehmicky@gmail.com>
2019-06-12 10:00:00 +0200

    Add option `ignoreNonConfigurable`

--- index.js
+++ index.js
@@ -2,15 +2,33 @@
 
 const {hasOwnProperty} = Object.prototype;
 
-const copyProperty = (to, from, property) => {
+const copyProperty = (to, from, property, {ignoreNonConfigurable = false}) => {
 	// `Function#length` should reflect the parameters of `to` not `from` since we keep its body.
 	// `Function#prototype` is non-writable and non-configurable so can never be modified.
 	if (property === 'length' || property === 'prototype') {
 		return;
 	}
 
-	const descriptor = Object.getOwnPropertyDescriptor(from, property);
-	Object.defineProperty(to, property, descriptor);
+	const toDescriptor = Object.getOwnPropertyDescriptor(to, property);
+	const fromDescriptor = Object.getOwnPropertyDescriptor(from, property);
+
+	if (!canCopyProperty(toDescriptor, fromDescriptor) && ignoreNonConfigurable) {
+		return;
+	}
+
+	Object.defineProperty(to, property, fromDescriptor);
+};
+
+// `Object.defineProperty()` throws if the property exists, is not configurable and either:
+//  - one its descriptors is changed
+//  - it is non-writable and its value is changed
+const canCopyProperty = function (toDescriptor, fromDescriptor) {
+	return toDescriptor === undefined || toDescriptor.configurable || (
+		toDescriptor.writable === fromDescriptor.writable &&
+		toDescriptor.enumerable === fromDescriptor.enumerable &&
+		toDescriptor.configurable === fromDescriptor.configurable &&
+		(toDescriptor.writable || toDescriptor.value === fromDescriptor.value)
+	);
 };
 
 const changePrototype = (to, from) => {
@@ -37,9 +55,9 @@ const removeProperty = (to, from, property) => {
 	}
 };
 
-const mimicFn = (to, from) => {
+const mimicFn = (to, from, opts = {}) => {
 	for (const property of Reflect.ownKeys(from)) {
-		copyProperty(to, from, property);
+		copyProperty(to, from, property, opts);
 	}
 
 	changePrototype(to, from);
